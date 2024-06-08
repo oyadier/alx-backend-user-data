d0cc83f82521abf1eaf1ebd1f69d1c5b7a250e70
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-06-08 02:16:46 +0000

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,9 +1,11 @@
-from flask import request
-from typing import List, TypeVar, Generic
+#!/usr/bin/env python3
 """An authentication Module"""
+from flask import request
+from typing import (List, TypeVar)
+from models.user import User
 
 
-class Auth:
+class Auth():
     '''An authentication class for all logins'''
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
@@ -17,5 +19,6 @@ class Auth:
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        '''Request for the current'''
+        '''Getting the current user
+        '''
         return None
