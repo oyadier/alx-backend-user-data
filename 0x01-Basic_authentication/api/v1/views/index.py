caf0f9c8bd9a9f7f034a0baef3360fb5a3fd68e2
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-06-08 14:59:42 +0000

    Request validation

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -6,59 +6,60 @@ import requests
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """GET /api/v1/status
     Return:
       - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route("/stats/", strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """GET /api/v1/stats
     Return:
       - the number of each objects
     """
     from models.user import User
+
     stats = {}
-    stats['users'] = User.count()
+    stats["users"] = User.count()
     return jsonify(stats)
 
 
 @app_views.errorhandler(401)
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
 def unauthorized() -> str:
-    """ GET /api/v1/error
+    """GET /api/v1/error
     Return:
       - the error message
     """
-    response = jsonify({'error': 'Unauthorized'})
+    response = jsonify({"error": "Unauthorized"})
     response.status_code = 401
     return response
 
 
 @app_views.errorhandler(403)
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
 def forbidden() -> str:
-    """ GET /api/v1/error
+    """GET /api/v1/error
     Return:
       - the error message (forbidden)
     """
-    response = jsonify({'error': 'Forbidden'})
+    response = jsonify({"error": "Forbidden"})
     response.status_code = 403
     return response
 
-    '''All custome Error handlers '''
+    """All custome Error handlers """
 
 
 @app_views.errorhandler(401)
 @app_views.errorhandler(403)
 def erorhandler(error):
-    """ Error code
-      Return:
-        the status code for the error
+    """Error code
+    Return:
+      the status code for the error
     """
     if error.code == 401:
         abort(401)
