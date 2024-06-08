d6a6628b70b8acd76d066aaa0806c95f33e7cd6e
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-06-08 03:08:15 +0000

    Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 """An authentication Module"""
 from flask import request
-from typing import (List, TypeVar)
+from typing import List, TypeVar
 from models.user import User
 
 
-class Auth():
-    '''An authentication class for all logins'''
+class Auth:
+    """An authentication class for all logins"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        '''Checks if the action requires an authentication
-        '''
-        return False
+        """Checks if the action requires an authentication"""
+        if path is not None:
+            if path.endswith("/"):
+                path = path[:-1]
+
+        if path is None or excluded_paths is None or len(excluded_paths) < 1:
+            return True
+        if path in excluded_paths or "/api/v1/status/" in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
-        '''The request auth header
-        '''
+        """The request auth header"""
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        '''Getting the current user
-        '''
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Getting the current user"""
         return None
