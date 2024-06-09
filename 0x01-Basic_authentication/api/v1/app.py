e90614f3152ab8bc059768b1ee63b5e4e5e3ded9
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-06-09 02:30:51 +0000

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS, cross_origin
-
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,8 +17,6 @@ if auth == "auth":
     from api.v1.auth.auth import Auth
 
     auth = Auth()
-
-from api.v1.auth.basic_auth import BasicAuth
 
 auth = BasicAuth()
 
