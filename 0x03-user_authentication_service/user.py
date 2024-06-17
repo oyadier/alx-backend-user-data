d554689f231035c29cfcd44e2fb947525f3f7843
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-06-17 05:14:17 +0000

    Add user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,7 @@ class User(Base):
     '''User table class for mapping
     '''
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
