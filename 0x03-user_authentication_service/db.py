44fc2dea9ab8e6e848aa0dc7e8c00b4752fc2a93
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-06-17 06:46:04 +0000

    New User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         '''Add user to the db
         '''
         user = User(email=email, hashed_password=hashed_password)
