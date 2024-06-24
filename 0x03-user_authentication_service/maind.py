fd095aea91c1b1bcdd32ab90596171a351e235a7
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2024-06-24 02:46:24 +0000

    Add User

--- 0x03-user_authentication_service/maind.py
+++ 0x03-user_authentication_service/maind.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""
+"""    
 Main file
 """
 
@@ -12,4 +12,5 @@ user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
 print(user_1.id)
 
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
+print("The outcome: {user_2.id}")
+
